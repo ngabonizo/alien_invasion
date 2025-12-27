2e43d02d4d21a8dcf72a9176699d704f42080190
ngabo <ngaboizo2000@gmail.com>
2025-12-27 12:01:14 +0200

    create images folder

--- elien_invasion.py
+++ elien_invasion.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 from settings import Settings
-class AlienInvasion:
+class ElienInvasion:
   # Overall class to manage game behavior
 
   def __init__(self):
@@ -14,9 +14,11 @@ class AlienInvasion:
       (self.settings.screen_width, self.settings.screen_height)
     )
     pygame.display.set_caption('Alien Invasion')
+
+    #this controlls framerate as allowed by the CPU
+    self.clock = pygame.time.Clock() 
   
- #set the background color
-    self.bg_color = (230, 230, 230)
+
   def run_game(self):
     #start the main loop for the game
     while True:
@@ -29,8 +31,9 @@ class AlienInvasion:
 
       #Make the most recently drawn screen visible.
       pygame.display.flip()
+      self.clock.tick(60)
 
 if __name__ == '__main__':
   #Make a game instance, and run the game.
-  ai = AlienInvasion()
+  ai = ElienInvasion()
   ai.run_game()
